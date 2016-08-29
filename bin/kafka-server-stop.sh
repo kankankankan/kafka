b499114db81fab5101bb833067ad2a3fa91c1dca
yanghua <yqh5566@163.com>
2016-08-29 12:32:06 +0800

    Update kafka-server-stop.sh

--- bin/kafka-server-stop.sh
+++ bin/kafka-server-stop.sh
@@ -13,7 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-PIDS=$(ps ax | grep -i 'kafka\.Kafka' | grep java | grep -v grep | awk '{print $1}')
+# 原有的pids不能返回kafka的pid，所以修改成以下方式，但需安装jps
+#PIDS=$(ps ax | grep -i 'kafka\.Kafka' | grep java | grep -v grep | awk '{print $1}') 
+JAVA_HOME=/usr/java/jdk1.7.0_21
+PIDS=$($JAVA_HOME/bin/jps -l | grep -i 'kafka\.Kafka' | awk '{print $1}')
 
 if [ -z "$PIDS" ]; then
   echo "No kafka server to stop"
